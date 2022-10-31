f520ac7acf7d9ed70c71bf5aaf8961148357aeb7
Joseph Soria <josephsoria6@gmail.com>
2022-10-30 22:05:50 -0500

    Wrote content for WelcomeView

--- src/components/WelcomeView.js
+++ src/components/WelcomeView.js
@@ -1,7 +1,18 @@
 const WelcomeView = () => {
   return (
-    <h1>This is a view</h1>
-  )
-}
+    <>
+      <header className="welcome-header">
+        <h1>Welcome!</h1>
+        <p>This is an interactive form written by Joseph Soria</p>
+        <p>
+          All information here is stored <em>locally</em>, and will not be
+          written to a database!
+        </p>
+        <p>Shall we begin?</p>
+      </header>
+      <button className="start-button"> Let's begin!</button>
+    </>
+  );
+};
 
 export default WelcomeView;
