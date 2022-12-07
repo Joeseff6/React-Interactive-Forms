e11ce155a1b8084dd59bed0bd6d5ec54bfe07edc
Joseph Soria <josephsoria6@gmail.com>
2022-12-06 21:55:44 -0600

    Edited welcoming text

--- src/components/WelcomeView.js
+++ src/components/WelcomeView.js
@@ -3,12 +3,12 @@ const WelcomeView = () => {
     <>
       <header className="welcome-header">
         <h1>Welcome!</h1>
-        <p>This is an interactive form written by Joseph Soria</p>
-        <p>
-          All information here is stored <em>locally</em>, and will not be
-          written to a database!
+        <p>The following is a set of forms that will ask basic questions about yourself</p>
+        <p>Feel free to answer how you please and enjoy!</p>
+        <p className="disclaimer">
+          Disclaimer: All information here is stored <em>locally</em>, and will
+          not be written to a database!
         </p>
-        <p>Shall we begin?</p>
       </header>
       <button className="start-button"> Let's begin!</button>
     </>
