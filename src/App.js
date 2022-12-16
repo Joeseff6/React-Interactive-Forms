8747a7da10871dc2ceb28e2bdc5c1b2674ad9136
Joseph Soria <josephsoria6@gmail.com>
2022-12-15 21:12:42 -0600

    Commented out WelcomeView

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import WelcomeView from "./components/WelcomeView";
+import FirstForm from "./components/FirstForm";
 import Wrapper from "./components/Wrapper";
 
 function App() {
   return (
     <div className="App">
       <Wrapper>
-        <WelcomeView />
+        {/* <WelcomeView /> */}
+        <FirstForm />
       </Wrapper>
     </div>
   );
